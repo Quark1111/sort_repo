1a02698b00d36b22d0a59ddaf56172bb773050e3
Quark1111 <tevrukovila@gmail.com>
2025-12-12 03:48:19 +0300

    Update QuickSort.py

--- src/QuickSort.py
+++ src/QuickSort.py
@@ -1,18 +1,20 @@
 def QuickSort(mas, l, r):
-    i = l
-    j = r
-    middle = (l + r) // 2
-    while (i <= j):
-        while (mas[i] < mas[middle] and i < r):
-            i += 1
-        while (mas[j] > mas[middle] and j > l):
-            j -= 1
-        if (i <= j):
-            mas[i], mas[j] = mas[j], mas[i]
-            i += 1
-            j -= 1
-    if (l < j):
-        QuickSort(mas, l, j)
-    if (i < r):
-        QuickSort(mas, i, r)
+    if l < r:
+        i = l
+        j = r
+        middle = (l + r) // 2
+        privot = mas[middle]
+        while (i <= j):
+            while (mas[i] < privot):
+                i += 1
+            while (mas[j] > privot):
+                j -= 1
+            if (i <= j):
+                mas[i], mas[j] = mas[j], mas[i]
+                i += 1
+                j -= 1
+        if (l <= j):
+            QuickSort(mas, l, j)
+        if (i < r):
+            QuickSort(mas, i, r)
     return mas
